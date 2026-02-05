a1aeebe59e291df513cacb7f880cd78f658ceeba
tuschuk <tristen.uschuk21@gmail.com>
2026-02-05 10:12:31 -0700

    added borders for home page

--- client/max-method/src/pages/home.jsx
+++ client/max-method/src/pages/home.jsx
@@ -1,5 +1,13 @@
 function Home () {
-    return <h1>Home</h1>
+    return <div className="home-container">
+            <h1>Home</h1>
+            <div className = "home-classification-section">
+                <h2>Current Fitness Level: </h2>
+            </div>
+            <div className = "home-program-section">
+                <h2>Program Section</h2>
+            </div>
+        </div>
 }
 
 export default Home;
