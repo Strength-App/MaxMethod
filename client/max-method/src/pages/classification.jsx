0f5da629f2a2d51f912da2abcc224dc30e04f4d3
Tony <59328777+Technut10@users.noreply.github.com>
2026-02-10 09:04:32 -0700

    update calendar

--- client/max-method/src/pages/classification.jsx
+++ client/max-method/src/pages/classification.jsx
@@ -1,5 +1,72 @@
 function Classification () {
-    return <h1>Classification</h1>
+    return (
+        <div className="classification-page">
+
+            {/* Classification Page Header */}
+            <header>
+                <h1>Classification</h1>
+            </header>
+
+            {/* Gender Selection and Data Input */}
+            <main>
+                <p className="subheading">Gender</p>
+
+                <div className="classification-gender-options">
+                    <label>
+                        <input type="radio" name="gender" /> Male
+                    </label>
+
+                    <label>
+                        <input type="radio" name="gender" /> Female
+                    </label>
+
+                    <label>
+                        <input type="radio" name="gender" /> Other
+                    </label>
+                </div>
+
+
+                <div className="classification-data-grid">
+                    <div className="classification-data">
+                        <p>Bench Press</p>
+                        <div className="input-unit">
+                            <input type="number" placeholder="Enter weight" />
+                            <span>lbs</span>
+                        </div>
+                    </div>
+
+                    <div className="classification-data">
+                        <p>Deadlift</p>
+                        <div className="input-unit">
+                            <input type="number" placeholder="Enter weight" />
+                            <span>lbs</span>
+                        </div>
+                    </div>
+
+                    <div className="classification-data">
+                        <p>Squat</p>
+                        <div className="input-unit">
+                            <input type="number" placeholder="Enter weight" />
+                            <span>lbs</span>
+                        </div>
+                    </div>
+
+                    <div className="classification-data">
+                        <p>Body Weight</p>
+                        <div className="input-unit">
+                            <input type="number" placeholder="Enter weight" />
+                            <span>lbs</span>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="classification-submit">
+                    <button type="submit">Submit</button>
+                </div>
+
+            </main>
+        </div>
+    )
 }
 
 export default Classification;
