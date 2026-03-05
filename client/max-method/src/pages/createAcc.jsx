7b3e4acabec17a599e4032c270c429945ad9664a
Anthony <technut.devops@gmail.com>
2026-03-05 15:41:08 -0700

    Update Create Account

--- client/max-method/src/pages/createAcc.jsx
+++ client/max-method/src/pages/createAcc.jsx
@@ -1,58 +1,112 @@
 import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
+import { useUser } from '../context/UserContext';
+
+import axios from "axios";
 
 
-function CreateAcc({ setIsAuthenticated }) {
-  const [name, setName] = useState('')
+function CreateAcc() {
+  const [ firstName, setFirstName] = useState('')
+  const [ lastName, setLastName] = useState('')
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
   const navigate = useNavigate()
+  const { setUser } = useUser();
 
-  const handleSubmit = (e) => {
-    e.preventDefault()
 
-    console.log({ name, email, password })
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const userData = {firstName, lastName, email, password};
+      const response = await axios.post('http://localhost:5050/api/users/create-account/', userData);
+      console.log("Status: ", response.status);
+      setFirstName('');
+      setLastName('');
+      setEmail('');
+      setPassword('');
+      setUser(response.data);
+      alert('Account created successfully');
+      navigate('/classification');
+    }catch (error) {
+      console.error('Error creating account:', error);
+      alert('Error creating account');
+    }
+
 
     //AUTHENTICATE USER
-    // setIsAuthenticated(true)
-
-    //GO TO HOME
-    navigate('/home', { replace: true })
+    // Check if all inputs are filled in and the password is at least 8 characters long
+    // if (!firstName || !lastName || !email || !password) {
+    //   alert('Please fill in all fields')
+    //   return
+    // }
+    // else if (password.length < 8) {
+    //      alert('Password must be at least 8 characters long')
+    //   return
+    // }
+    // else{
+    //
+    //   submitForm(e).then(response => {
+    //     console.log(response.data);
+    //   })
+    //
+    // }
+    console.log({ firstName, lastName, email, password })
+    //GO TO goals
   }
+  // const submitForm = async (e) => {
+  //   e.preventDefault();
+  //   await axios.post('http://localhost:5050/api/users/', {email: email, password: password, firstName: firstName, lastName: lastName})
+  //       .then(response => {
+  //         console.log(response.data);
+  //         navigate('/classification');
+  //       })
+  // }
 
   return (
-    <div className="create-account-page">
-      <h1>Create Account</h1>
+      <div className="create-account-page">
+        <h1>Create Account</h1>
 
-      <form onSubmit={handleSubmit} className="create-account">
-        <input
-          type="text"
-          placeholder="Name"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-          required
-        />
+        <form onSubmit={handleSubmit} className="create-account">
+          <input
+              type="text"
+              placeholder="First Name"
+              value={firstName}
+              onChange={(e) => setFirstName(e.target.value)}
+              required
+          />
+          <br/>
+          <input
+              type="text"
+              placeholder="Last Name"
+              value={lastName}
+              onChange={(e) => setLastName(e.target.value)}
+              required
+          />
+          <br/>
+          <input
+              type="email"
+              placeholder="Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+          />
+          <br/>
+          <input
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+          />
 
-        <input
-          type="email"
-          placeholder="Email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          required
-        />
-
-        <input
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-        />
-
-        <button type="submit">Create Account</button>
-      </form>
-    </div>
+          <button className="history-button" type="submit">Create Account</button>
+        </form>
+      </div>
   )
 }
+
+
+
+
 
 export default CreateAcc
